018940c9043ac99903c4674ed12ba5d66da9a9a9
Sid Srinivasan <Sid.srinivasan@amd.com>
2025-08-25 15:01:00 -0700

    run_two_cmds.py

---
+++ run_two_cmds.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+#
+# Copyright (c) 2023 Advanced Micro Devices, Inc. All Rights Reserved.
+#
+# Author: Sid Srinivasan (sid.srinivasan@amd.com)
+# Check all rocm driver packages installed
+# v1.0 Initial Version
+import subprocess
+
+def run_two_cmds():
+    cmd1 = ["dpkg", "-l"]
+    cmd2 = ["grep", "amdgpu"] 
+    p1 = subprocess.Popen(cmd1, stdout=subprocess.PIPE)
+    p2 = subprocess.Popen(cmd2, stdin=p1.stdout, stdout=subprocess.PIPE)
+    p1.stdout.close()
+    output = p2.communicate()[0]
+    return output.decode('utf-8')
+
+if __name__ == "__main__":
+    print(run_two_cmds())
