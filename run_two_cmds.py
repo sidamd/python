0dda86ed48d919a3cff6ea2d4810bbafe80d3f2a
Sid Srinivasan <Sid.srinivasan@amd.com>
2025-08-25 16:10:02 -0700

    add cmdline argument support

--- run_two_cmds.py
+++ run_two_cmds.py
@@ -6,6 +6,7 @@
 # Check all rocm driver packages installed
 # v1.0 Initial Version
 import subprocess
+import argparse
 
 def run_two_cmds(cmd1, cmd2):
     p1 = subprocess.Popen(cmd1, stdout=subprocess.PIPE)
@@ -15,6 +16,12 @@ def run_two_cmds(cmd1, cmd2):
     return output.decode('utf-8')
 
 if __name__ == "__main__":
-    cmd1 = "dpkg -l"
-    cmd2 = "grep amdgpu"
-    print(run_two_cmds(cmd1.split(), cmd2.split()))
+    parser = argparse.ArgumentParser(description='Enter two commands')
+    parser.add_argument('--cmd1', nargs=1, dest='cmd1', required=True, help='cmd 1')
+    parser.add_argument('--cmd2', nargs=1, dest='cmd2', required=True, help='cmd 2')
+    args = parser.parse_args()
+
+#    print("--cmd1 = ", args.cmd1[0])
+#    print("--cmd2 = ", args.cmd2[0])
+
+    print(run_two_cmds(args.cmd1[0].split(), args.cmd2[0].split()))
